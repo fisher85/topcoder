b1774d3bffb7d8281f390bf68a9ad1678200a4e0
fisher85 <79208085372@ya.ru>
2019-05-04 08:48:09 +0300

    Add .NET Core helloworld app

---
+++ 2019-05-04 .NET Core example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2019_05_04_.NET_Core_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
