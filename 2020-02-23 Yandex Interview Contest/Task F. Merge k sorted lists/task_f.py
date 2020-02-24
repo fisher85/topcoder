5b81b894b51593a7c4415e3e997685f23ec47d46
fisher85 <79208085372@ya.ru>
2020-02-24 14:46:03 +0300

    Add Python Read-Write Examples.py

--- 2020-02-23 Yandex Interview Contest/Task F. Merge k sorted lists/task_f.py
+++ 2020-02-23 Yandex Interview Contest/Task F. Merge k sorted lists/task_f.py
@@ -1,4 +1,31 @@
+# 1: TL - 1.09s 4.32Mb test № 17
+# 2, save prev position: 
+# 3, sort at startup: 
+
+k = 0
 a = []
+
+def diho(arr, element, previous_position):
+    if element < arr[0]:
+        arr.insert(0, element)
+        return 0
+    if element > arr[len(arr) - 1]:
+        arr.insert(len(arr), element)
+        return len(arr) - 1
+
+    left = 0
+    right = len(arr) - 1
+
+    while (left < right):
+        mid = int((right - left)/2 + left)
+        if (element > arr[mid]):
+            left = mid + 1
+        else:
+            right = mid
+
+    arr.insert(left, element)
+    return left
+
 with open("c:/topcoder/topcoder/2020-02-23 Yandex Interview Contest/Task F. Merge k sorted lists/input.txt", "r") as fr:
     input_lines = fr.readlines()
     k = int(input_lines[0])
@@ -9,4 +36,11 @@ with open("c:/topcoder/topcoder/2020-02-23 Yandex Interview Contest/Task F. Merg
         b.pop(0)
         a.append(b)
 
+if (k > 0):
+    result = a[0].copy()
+    previous_position = 0
+    for i in range(1, k):
+        for j in a[i]:
+            previous_position = diho(result, j, previous_position)
 
+    print(*result, sep=' ')
---
+++ Python Read-Write Examples.py
@@ -0,0 +1,61 @@
+# Чтение из файла
+fr = open("input.txt", "r")
+line = fr.readline()
+parts = line.split()
+
+###
+with open("2019-05-10 Yandex ML blitz 2018/input1_1.csv", "r") as fr:
+      input_lines = fr.readlines()
+      b = int(input_lines[0])
+      X = [[int(num) for num in line.split(",")] for line in input_lines[1:]]
+
+###
+with open(file_name, "r") as fr:
+    data_orig = [[float(num) for num in line.split()] for line in fr]
+
+###
+with open(file_input, "r") as fr, open(file_output, "w") as fw:
+    input_lines = fr.readlines()
+    n, m = map(int, input_lines[0].strip().split())
+    
+    # Первое значение всегда идет в output
+    prev = int(input_lines[1])
+    fw.write("0 ")
+
+###
+with open(input_file, "r") as fr:
+    data_orig = [[float(num) for num in line.split()] for line in fr]
+
+###
+with open(input_file, "r") as fr:
+    input_lines = fr.readlines()
+    n, m, k = map(int, input_lines[0].strip().split())
+    orig = [[int(num) for num in line.strip().split()] for line in input_lines[1:n+1]]
+    req = int(input_lines[n+1].strip().replace(' ', ''))
+
+
+
+# Запись в файл
+fw = open("output.txt", "w")
+fw.write(result)
+
+###
+output_name = "2019-05-12 Yandex ML blitz 2018 final/output_A_12.txt"
+with open(output_name, "w") as fw:
+    fw.write(str.format("{0:.6f}", a) + " ")
+    fw.write(str.format("{0:.6f}", b) + " ")
+    fw.write(str.format("{0:.6f}", c))
+
+###
+print(*result, sep=' ')
+
+###
+print(a[0,:])
+print(format(np.sum(a[0,:]), ".8f"))
+print(b[0])
+
+###
+with open(input_name, "r") as fr:
+    test_data = [[float(num) for num in line.split()] for line in fr]
+result = np.sum(test_data, axis=1)
+np.savetxt(output_name, result, fmt='%0.8f')
